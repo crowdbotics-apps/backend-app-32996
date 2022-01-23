ca2e6fffb9363c14afaf61ed1f2d0d596153a1e0
60sameed <60sameed@gmail.com>
2022-01-24 04:18:51 +0500

    Some changes.

--- home/api/v1/viewsets.py
+++ home/api/v1/viewsets.py
@@ -44,67 +44,54 @@ class AppViewSet(ModelViewSet):
     permission_classes = (permissions.IsAuthenticated,)
 
     def list(self, request, *args, **kwargs):
-        try:
-            token = Token.objects.get(key=request.auth.key)
-            apps = App.objects.filter(user=token.user_id)
-            return Response(data=AppSerializer(apps, many=True).data)
-        except:
-            return Response(data={"message": f"Internal Server Error."}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+        token = Token.objects.get(key=request.auth.key)
+        apps = App.objects.filter(user=token.user_id)
+        return Response(data=AppSerializer(apps, many=True).data)
+
 
     def retrieve(self, request, *args, **kwargs):
-        try:
-            app_id = kwargs['pk']
-            app = App.objects.filter(id=app_id).first()
-            token = Token.objects.get(key=request.auth.key)
-            if app is None:
-                return Response(data={"message": f"No App found against id {app_id}."}, status=status.HTTP_404_NOT_FOUND)
-            if app.user_id != token.user_id:
-                return Response(data={"message": f"User is not authorized to retrieve app having id {app_id}."},status=status.HTTP_403_FORBIDDEN)
-            return Response(data=AppSerializer(app).data)
-        except:
-            return Response(data={"message": f"Internal Server Error."}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+
+        app_id = kwargs['pk']
+        app = App.objects.filter(id=app_id).first()
+        token = Token.objects.get(key=request.auth.key)
+        if app is None:
+            return Response(data={"message": f"No App found against id {app_id}."}, status=status.HTTP_404_NOT_FOUND)
+        if app.user_id != token.user_id:
+            return Response(data={"message": f"User is not authorized to retrieve app having id {app_id}."},status=status.HTTP_403_FORBIDDEN)
+        return Response(data=AppSerializer(app).data)
 
     def create(self, request, *args, **kwargs):
-        try:
-            request.data['user'] = Token.objects.get(key=request.auth.key).user_id
-            serializer = self.serializer_class(
-                data=request.data, context={"request": request}
-            )
-            serializer.is_valid(raise_exception=True)
-            return super().create(request, *args, **kwargs)
-        except:
-            return Response(data={"message": f"Internal Server Error."}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+        request.data['user'] = Token.objects.get(key=request.auth.key).user_id
+        serializer = self.serializer_class(
+            data=request.data, context={"request": request}
+        )
+        serializer.is_valid(raise_exception=True)
+        return super().create(request, *args, **kwargs)
 
     def update(self, request, *args, **kwargs):
-        try:
-            app_id = kwargs['pk']
-            app = App.objects.filter(id=app_id).first()
-            token = Token.objects.get(key=request.auth.key)
-            if app is None:
-                return Response(data={"message": f"No App found to update against id {app_id}."}, status=status.HTTP_404_NOT_FOUND)
-            if app.user_id != token.user_id:
-                return Response(data={"message": f"User is not authorized to modify app having id {app_id}."}, status=status.HTTP_403_FORBIDDEN)
-            serializer = self.serializer_class(
-                data=request.data, context={"request": request}
-            )
-            serializer.is_valid(raise_exception=True)
-            return super().update(request, *args, **kwargs)
-        except:
-            return Response(data={"message": f"Internal Server Error."}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+        app_id = kwargs['pk']
+        app = App.objects.filter(id=app_id).first()
+        token = Token.objects.get(key=request.auth.key)
+        if app is None:
+            return Response(data={"message": f"No App found to update against id {app_id}."}, status=status.HTTP_404_NOT_FOUND)
+        if app.user_id != token.user_id:
+            return Response(data={"message": f"User is not authorized to modify app having id {app_id}."}, status=status.HTTP_403_FORBIDDEN)
+        serializer = self.serializer_class(
+            data=request.data, context={"request": request}
+        )
+        serializer.is_valid(raise_exception=True)
+        return super().update(request, *args, **kwargs)
 
     def destroy(self, request, *args, **kwargs):
-        try:
-            app_id = kwargs['pk']
-            app = App.objects.filter(id=app_id).first()
-            token = Token.objects.get(key=request.auth.key)
-            if app is None:
-                return Response(data={"message": f"No App found to delete against id {app_id}."}, status=status.HTTP_404_NOT_FOUND)
-            if app.user_id != token.user_id:
-                return Response(data={"message": f"User is not authorized to retrieve app having id {app_id}."}, status=status.HTTP_403_FORBIDDEN)
-            App.objects.filter(id=app_id).delete()
-            return Response(data={"message": f"App deleted successfully."})
-        except :
-            return Response(data={"message": f"Internal Server Error."}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+        app_id = kwargs['pk']
+        app = App.objects.filter(id=app_id).first()
+        token = Token.objects.get(key=request.auth.key)
+        if app is None:
+            return Response(data={"message": f"No App found to delete against id {app_id}."}, status=status.HTTP_404_NOT_FOUND)
+        if app.user_id != token.user_id:
+            return Response(data={"message": f"User is not authorized to retrieve app having id {app_id}."}, status=status.HTTP_403_FORBIDDEN)
+        App.objects.filter(id=app_id).delete()
+        return Response(data={"message": f"App deleted successfully."})
 
 
 class PlanViewSet(ModelViewSet):
@@ -113,13 +100,6 @@ class PlanViewSet(ModelViewSet):
     queryset = Plan.objects.all()
     http_method_names = ["get"]
     permission_classes = (permissions.IsAuthenticated,)
-
-    def retrieve(self, request, *args, **kwargs):
-        try:
-            plan_id = kwargs.get('pk')
-            return super().retrieve(request, *args, **kwargs)
-        except Http404:
-            return Response(data={"message": f"No plan found against id {plan_id}."}, status=status.HTTP_404_NOT_FOUND)
 
 
 class SubscriptionViewSet(ModelViewSet):
@@ -130,54 +110,41 @@ class SubscriptionViewSet(ModelViewSet):
 
 
     def list(self, request, *args, **kwargs):
-        try:
-            token = Token.objects.get(key=request.auth.key)
-            subscriptions = Subscription.objects.filter(user=token.user_id)
-            return Response(data=SubscriptionSerializer(subscriptions, many=True).data)
-        except:
-            return Response(data={"message": f"Internal Server Error."}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+        token = Token.objects.get(key=request.auth.key)
+        subscriptions = Subscription.objects.filter(user=token.user_id)
+        return Response(data=SubscriptionSerializer(subscriptions, many=True).data)
 
     def retrieve(self, request, *args, **kwargs):
-        try:
-            subscription_id = kwargs['pk']
-            subscription = Subscription.objects.filter(id=subscription_id).first()
-            token = Token.objects.get(key=request.auth.key)
-            if subscription is None:
-                return Response(data={"message": f"No subscription found against id {subscription_id}."}, status=status.HTTP_404_NOT_FOUND)
-            if subscription.user_id != token.user_id:
-                return Response(data={"message": f"User is not authorized to retrieve subscription having id {subscription_id}."},status=status.HTTP_403_FORBIDDEN)
-            return Response(data=SubscriptionSerializer(subscription).data)
-        except:
-            return Response(data={"message": f"Internal Server Error."}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+        subscription_id = kwargs['pk']
+        subscription = Subscription.objects.filter(id=subscription_id).first()
+        token = Token.objects.get(key=request.auth.key)
+        if subscription is None:
+            return Response(data={"message": f"No subscription found against id {subscription_id}."}, status=status.HTTP_404_NOT_FOUND)
+        if subscription.user_id != token.user_id:
+            return Response(data={"message": f"User is not authorized to retrieve subscription having id {subscription_id}."},status=status.HTTP_403_FORBIDDEN)
+        return Response(data=SubscriptionSerializer(subscription).data)
 
     def create(self, request, *args, **kwargs):
-        try:
-            # override user id which is creating user
-            request.data['user'] = Token.objects.get(key=request.auth.key).user_id
-            serializer = self.serializer_class(
-                data=request.data, context={"request": request}
-            )
-            serializer.is_valid(raise_exception=True)
-            return super().create(request, *args, **kwargs)
-        except:
-            return Response(data={"message": f"Internal Server Error."}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+        # override user id which is creating user
+        request.data['user'] = Token.objects.get(key=request.auth.key).user_id
+        serializer = self.serializer_class(
+            data=request.data, context={"request": request}
+        )
+        serializer.is_valid(raise_exception=True)
+        return super().create(request, *args, **kwargs)
 
 
     def update(self, request, *args, **kwargs):
-        try:
-            subscription_id = kwargs['pk']
-            subscription = Subscription.objects.filter(id=subscription_id).first()
-            token = Token.objects.get(key=request.auth.key)
-            if subscription is None:
-                return Response(data={"message": f"No subscription found against id {subscription_id}."}, status=status.HTTP_404_NOT_FOUND)
-            if subscription.user_id != token.user_id:
-                return Response(data={"message": f"User is not authorized to update subscription having id {subscription_id}."}, status=status.HTTP_403_FORBIDDEN)
-            request.data['user'] = token.user_id
-            serializer = self.serializer_class(
-                data=request.data, context={"request": request}
-            )
-            serializer.is_valid(raise_exception=True)
-            return super().update(request, *args, **kwargs)
-        except Exception as e:
-            print(e)
-            return Response(data={"message": f"Internal Server Error."}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+        subscription_id = kwargs['pk']
+        subscription = Subscription.objects.filter(id=subscription_id).first()
+        token = Token.objects.get(key=request.auth.key)
+        if subscription is None:
+            return Response(data={"message": f"No subscription found against id {subscription_id}."}, status=status.HTTP_404_NOT_FOUND)
+        if subscription.user_id != token.user_id:
+            return Response(data={"message": f"User is not authorized to update subscription having id {subscription_id}."}, status=status.HTTP_403_FORBIDDEN)
+        request.data['user'] = token.user_id
+        serializer = self.serializer_class(
+            data=request.data, context={"request": request}
+        )
+        serializer.is_valid(raise_exception=True)
+        return super().update(request, *args, **kwargs)
